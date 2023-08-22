91e0bb59bfd6a941ff54dbb5b23e86fbb576df89
MihailIlyushenkov <142346042+MihailIlyushenkov@users.noreply.github.com>
2023-08-22 16:32:45 +0300

    Update firsttask.cpp

--- firsttask.cpp
+++ firsttask.cpp
@@ -4,11 +4,11 @@
 
 int main(void)
 {
-    double a, b, c, val0, val1;
-    int outpflag;
+    double a = 0, b = 0, c = 0, val0 = 0, val1 = 0;
+    int outpflag = 3, pres = 1;
 
-    input(&a, &b, &c);
-    solver(a, b, c, &outpflag, &val0, &val1);
-    output(&val0, &val1, &outpflag);
+    input(&a, &b, &c, &pres);
+    extdef(a, b, c, &outpflag, &val0, &val1);
+    output(&val0, &val1, &outpflag, pres);
     return 0;
 }
