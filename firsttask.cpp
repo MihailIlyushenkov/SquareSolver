c00488ece1f79e85ed6c6ce4c5ce976478d18f3d
MihailIlyushenkov <142346042+MihailIlyushenkov@users.noreply.github.com>
2023-08-23 16:00:46 +0300

    Update firsttask.cpp

--- firsttask.cpp
+++ firsttask.cpp
@@ -2,13 +2,26 @@
 #include <math.h>
 #include "myfunctions.h"
 
+
+// Сделать проверки на входе в каждую функцию
+// Почитать про doxygen
+// Фикс кодстайла ip => ptr, ... сделал
+// Make почитать
+
 int main(void)
 {
-    double a = 0, b = 0, c = 0, val0 = 0, val1 = 0;
-    int outpflag = 3, pres = 1;
+    /* double a = 0, b = 0, c = 0, val0 = 0, val1 = 0;
+    rootnum outpflag;
+    int prec = 1;
 
-    input(&a, &b, &c, &pres);
-    extdef(a, b, c, &outpflag, &val0, &val1);
-    output(&val0, &val1, &outpflag, pres);
+    input(&a, &b, &c);
+    prec = precision_input();
+    eqsdef(a, b, c, &outpflag, &val0, &val1);
+    output(&val0, &val1, outpflag, prec); */
+
+    EquasionTester();
+
+    getchar();
+
     return 0;
 }
