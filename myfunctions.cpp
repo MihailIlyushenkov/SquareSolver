5edd316cf6172710f938c8964e7d22adf410d4b5
MihailIlyushenkov <142346042+MihailIlyushenkov@users.noreply.github.com>
2023-08-23 16:00:44 +0300

    Update myfunctions.cpp

--- myfunctions.cpp
+++ myfunctions.cpp
@@ -1,13 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 #include <assert.h>
+#include <string.h>
 #include "myfunctions.h"
-
-#define NOROOTS 0
-#define INFROOTS -1
-#define ONEROOT 1
-#define TWOROOTS 2
-#define TWOCMPROOTS -2
 
 void cleaner(void)
 {
@@ -24,14 +19,19 @@ bool iszero(double x)
     return (abs(x) < EPS);
 }
 
-void input(double *a_ip, double *b_ip, double *c_ip, int *pres_ip)
+void input(double *a_ptr, double *b_ptr, double *c_ptr)
 {
+
+    assert(a_ptr != NULL);
+    assert(b_ptr != NULL);
+    assert(c_ptr != NULL);
+
     int counter = 0;
     printf("enter a, b, c in ax^2 + bx + c equation separated by a space\n");
 
     while (counter != 3)
     {
-        counter = scanf("%lf%lf%lf", a_ip, b_ip, c_ip);
+        counter = scanf("%lf%lf%lf", a_ptr, b_ptr, c_ptr);
         cleaner();
         if (counter != 3)
         {
@@ -39,102 +39,185 @@ void input(double *a_ip, double *b_ip, double *c_ip, int *pres_ip)
         }
     }
 
-    printf("enter precision for answers\n");
-    while (counter != 1)
-    {
-        counter = scanf("%d", pres_ip);
-        cleaner();
-        if (counter != 1)
-        {
-            printf("wrong input format, try again\n");
-        }
-    }
-
 }
 
-void extdef(double a, double b, double c, int *outpflag_ip, double *val0_ip, double *val1_ip)
+void eqsdef(double a, double b, double c, rootnum *outpflag_ptr, double *val0_ptr, double *val1_ptr)
 {
+
+    assert(outpflag_ptr != NULL);
+    assert(val0_ptr != NULL);
+    assert(val1_ptr != NULL);
+
     if (iszero(a))
     {
-        linearsolver(b, c, val0_ip, outpflag_ip);
+        linearsolver(b, c, val0_ptr, outpflag_ptr);
     }
     else
     {
-        squaresolver(a, b, c, outpflag_ip, val0_ip, val1_ip);
+        squaresolver(a, b, c, outpflag_ptr, val0_ptr, val1_ptr);
     }
 }
 
-void linearsolver(double a, double b, double *val0_ip, int *outpflag_ip)
+void linearsolver(double a, double b, double *val0_ptr, rootnum *outpflag_ptr)
 {
+    assert(val0_ptr != NULL);
+    assert(outpflag_ptr != NULL);
+
     if (iszero(a))
         {
             if (iszero(b))
             {
-                *outpflag_ip = INFROOTS; /* значение для вывода ответа "беск. кол-ва корней" */
+                *outpflag_ptr = INFROOTS; /* значение для вывода ответа "беск. кол-ва корней" */
                 return;
             }
             else
             {
-                *outpflag_ip = NOROOTS; /* значение для вывода ответа "нет корней"  */
+                *outpflag_ptr = NOROOTS; /* значение для вывода ответа "нет корней"  */
                 return;
             }
         }
         else
         {
-            *outpflag_ip = ONEROOT; /* значение для вывода ответа "есть 1 корень" */
-            *val0_ip = (-b)/a;
+            *outpflag_ptr = ONEROOT; /* значение для вывода ответа "есть 1 корень" */
+            *val0_ptr = (-b)/a;
             return;
         }
 }
 
-void squaresolver(double a, double b, double c, int *outpflag_ip, double *val0_ip, double *val1_ip)
+void squaresolver(double a, double b, double c, rootnum *outpflag_ptr, double *val0_ptr, double *val1_ptr)
 {
 
-    assert(val0_ip != NULL);
-    assert(val1_ip != NULL);
-    assert(outpflag_ip != NULL);
+    assert(val0_ptr != NULL);
+    assert(val1_ptr != NULL);
+    assert(outpflag_ptr != NULL);
     assert( !(iszero(a)) );
 
     if (iszero(a))
     {
-        linearsolver(b, c, val0_ip, outpflag_ip);
+        linearsolver(b, c, val0_ptr, outpflag_ptr);
         return;
     }
 
-    double discr = pow(b, 2) - 4.0*(a*c);
-    *val0_ip = (-b)/(2.0*a);
-    *val1_ip = sqrt(abs(discr))/(2.0*a);
+    // Лучшк b * b - сделал
+    double discr = b*b - 4 * a * c;
+    *val0_ptr = (-b) / (2.0 * a);
+    *val1_ptr = sqrt(abs(discr)) / (2.0 * a);
 
     if (iszero(discr))
     {
-        *outpflag_ip = ONEROOT; /* значение для вывода ответа "есть 1 действ. корень" */
+        *outpflag_ptr = ONEROOT; /* значение для вывода ответа "есть 1 действ. корень" */
     }
     else
     {
         if ( discr > 0 )
         {
-            *outpflag_ip = TWOROOTS; /* значение для вывода ответа "есть 2 действ. корня" */
+            *outpflag_ptr = TWOROOTS; /* значение для вывода ответа "есть 2 действ. корня" */
         }
         else
         {
-            *outpflag_ip = TWOCMPROOTS; /* значение для вывода ответа "есть 2 комплексных корня" */
+            *outpflag_ptr = TWOCMPROOTS; /* значение для вывода ответа "есть 2 комплексных корня" */
         }
     }
 
 }
 
-void output(double *val0_ip, double *val1_ip, int *outpflag_ip, int pres)
+void output(double *val0_ptr, double *val1_ptr, rootnum outpflag_ptr, int prec)
 {
-    switch(*outpflag_ip)
+    switch(outpflag_ptr)
     {
+
         case INFROOTS: printf("infitely many solutions"); break;
         case NOROOTS: printf("no solutions"); break;
-        case ONEROOT:  printf("solution is %.*lf", pres, *val0_ip); break;
-        case TWOROOTS:  printf("solutions are %.*lf and %.*lf", pres, *val0_ip + *val1_ip,
-            pres, *val0_ip - *val1_ip); break;
+        case ONEROOT:  printf("solution is %.*lf", prec, *val0_ptr); break;
+        case TWOROOTS:  printf("solutions are %.*lf and %.*lf", prec, *val0_ptr + *val1_ptr,
+            prec, *val0_ptr - *val1_ptr); break;
         case TWOCMPROOTS:  printf("solutions are %.*lf+%.*lfi and %.*lf-%.*lfi",
-            pres, *val0_ip, pres, *val1_ip, pres, *val0_ip, pres, *val1_ip); break;
+            prec, *val0_ptr, prec, *val1_ptr, prec, *val0_ptr, prec, *val1_ptr); break;
+        default: printf("output error");
     }
 }
 
+int precision_input()
+{
+    int prec = 0;
+    int counter = 0;
+    printf("enter precision for answers\n");
+    while (counter != 1)
+    {
+        counter = scanf("%d", &prec);
+        cleaner();
+        if ( (counter != 1) || ( prec < 0) )
+        {
+            printf("wrong input format, try again\n");
+        }
+    }
+    return prec;
+}
 
+rootnum Convertstringtoenum(char Enumstring[])
+{
+    if (!(strcmp(Enumstring, "INFROOTS")))
+    {
+        return INFROOTS;
+    }
+    else if (!(strcmp(Enumstring, "NOROOTS")))
+    {
+        return NOROOTS;
+    }
+    else if (!(strcmp(Enumstring, "ONEROOT")))
+    {
+        return ONEROOT;
+    }
+    else if (!(strcmp(Enumstring, "TWOROOTS")))
+    {
+        return TWOROOTS;
+    }
+    else if (!(strcmp(Enumstring, "TWOCMPROOTS")))
+    {
+        return TWOCMPROOTS;
+    }
+    return NOROOTS;
+}
+
+void TestOneEq(double dat[], rootnum outpflag_ref)
+{
+    double a = dat[0];
+    double b = dat[1];
+    double c = dat[2];
+    double val1_ref = dat[3];
+    double val2_ref = dat[4];
+    double val1 = 0, val2 = 0;
+    rootnum outpflag = NOROOTS;
+
+    squaresolver(a, b, c, &outpflag, &val1, &val2);
+
+    if ( iszero(val1 - val1_ref) || iszero(val2 - val2_ref) || iszero(outpflag != outpflag_ref) )
+    {
+        printf("Test Failed!!! Expected: val1 = %lf, val2 = %lf.n Exected: val1 = %lf, val2 = %lf \n", val1_ref, val2_ref, val1, val2);
+    }
+    else
+        printf("Test passed succesfully.\n");
+}
+
+int EquasionTester()
+{
+
+    const int TestCount = 3; /* Количество строк в файле с тестами */
+
+    FILE *file = fopen("C:\\Users\\Mi\\Documents\\GitHub\\SquareSolver\\Testdat.txt", "r");
+
+    for (int i = 0; i < TestCount; i += 1)
+    {
+        char Enumstring[12] = {0};
+        double dat1[6] = {0};
+        rootnum outpflag;
+
+        fscanf(file, "%lf %lf %lf %lf %lf %s",
+        &dat1[0], &dat1[1], &dat1[2], &dat1[3], &dat1[4], Enumstring);
+
+        outpflag = Convertstringtoenum(Enumstring);
+
+        TestOneEq(dat1[], outpflag);
+    }
+    return 0;
+}
