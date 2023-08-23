b2abcccfdd624c505d34ed32951c73369bd9291a
MihailIlyushenkov <142346042+MihailIlyushenkov@users.noreply.github.com>
2023-08-23 16:00:41 +0300

    Update myfunctions.h

--- myfunctions.h
+++ myfunctions.h
@@ -1,31 +1,45 @@
 #ifndef MYFUNCTIONS_H
 #define MYFUNCTIONS_H
 
+enum rootnum
+{
+    INFROOTS = -1, NOROOTS, ONEROOT, TWOROOTS, TWOCMPROOTS
+};
+
 void cleaner(void);
 /* ощищает поток ввода */
 
 bool iszero(double x);
 /* сравнивает double x с заранее заданной констатной EPS, близкой к 0. Выдает 1 если x < EPS, иначе 0) */
 
-void extdef(double a, double b, double c, int *outpflag_ip, double *val0_ip, double *val1_ip);
+void eqsdef(double a, double b, double c, rootnum *outpflag_ptr, double *val0_ptr, double *val1_ptr);
 /* вызывает linearsolver или squaresolver в зависимости от параметров a, b, c*/
 
-void input(double *a_ip, double *b_ip, double *c_ip, int *pres_ip);
+void input(double *a_ptr, double *b_ptr, double *c_ptr);
 /* обрабатывает ввод. При неверных данных просит ввести их повторно. Также вызывает cleaner() */
 
-void linearsolver(double a, double b, double *val0_ip, int *outpflag_ip);
+void linearsolver(double a, double b, double *val0_ptr, rootnum *outpflag_ptr);
 /* решает квадратное уравнение ax + b = 0.
-Записывает корень, если таковой имеется в val0_ip и служебную переменную для вывода в *outpflag_ip */
+    Записывает корень, если таковой имеется в val0_ptr и служебную переменную для вывода в *outpflag_ptr */
 
-void squaresolver(double a, double b, double c, int *outpflag_ip, double *val0_ip, double *val1_ip);
+void squaresolver(double a, double b, double c, rootnum *outpflag_ptr, double *val0_ptr, double *val1_ptr);
 /* Выполняет вычисления для решения уравнения. Получает на ввод данные коэффиценты a, b, c,
-а также указатели outpflag_ip - служебную переменную для функции output() и
-val0_ip (= -b/2a) и val1_ip (= sqrt(b^2 - 4ac)/2a ) -
+    а также указатели outpflag_ptr - служебную переменную для функции output() и
+    val0_ptr (= -b/2a) и val1_ptr (= sqrt(b^2 - 4ac)/2a ) -
     данные для вывода в функции  output(), которые записывает в процессе работы */
 
-void output(double *val0_ip, double *val1_ip, int *outpflag_ip, int pres);
+void output(double *val0_ptr, double *val1_ptr, rootnum outpflag_ptr, int prec);
 /* функция вывода данных в консоль из функции solver().
-Получает на ввод указатели на служебную переменную и 2 значения для вывода. */
+    Получает на ввод указатели на служебную переменную и 2 значения для вывода. */
 
+int precision_input();
+/* возвращает 1 натуральное число, введенное с клавиатуры */
 
+int EquasionTester(void);
+/*Вызыывает функцию TestOneEq количество TestCounter раз и передает ей данные
+    из Testdata.txt*/
+
+void TestOneEq(double dat[], rootnum outpfag_ref);
 #endif /*MYFUNCTIONS_H*/
+
+rootnum Convertstringtoenum(char Enumstring[]);
