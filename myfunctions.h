d68c2820beec9bdd9236b8c3657ea5bc52d0e4f5
MihailIlyushenkov <142346042+MihailIlyushenkov@users.noreply.github.com>
2023-08-24 10:10:08 +0300

    Update myfunctions.h

--- myfunctions.h
+++ myfunctions.h
@@ -8,7 +8,7 @@ enum rootnum
 
 enum errortype
 {
-    NOERROR = 0, FILENOPEN
+    NOERROR = 0, FILENOPEN, PTRERRORSQ, PTRERRORLN, PTRERRTESTF, PTRERRORINP
 };
 
 void cleaner(void);
@@ -17,17 +17,17 @@ void cleaner(void);
 bool iszero(double x);
 /* сравнивает double x с заранее заданной констатной EPS, близкой к 0. Выдает 1 если x < EPS, иначе 0) */
 
-void eqsdef(double a, double b, double c, rootnum *outpflag_ptr, double *val0_ptr, double *val1_ptr);
+errortype eqsdef(double a, double b, double c, rootnum *outpflag_ptr, double *val0_ptr, double *val1_ptr);
 /* вызывает linearsolver или squaresolver в зависимости от параметров a, b, c*/
 
-void input(double *a_ptr, double *b_ptr, double *c_ptr);
+errortype input(double *a_ptr, double *b_ptr, double *c_ptr);
 /* обрабатывает ввод. При неверных данных просит ввести их повторно. Также вызывает cleaner() */
 
-void linearsolver(double a, double b, double *val0_ptr, rootnum *outpflag_ptr);
+errortype linearsolver(double a, double b, double *val0_ptr, rootnum *outpflag_ptr);
 /* решает квадратное уравнение ax + b = 0.
     Записывает корень, если таковой имеется в val0_ptr и служебную переменную для вывода в *outpflag_ptr */
 
-void squaresolver(double a, double b, double c, rootnum *outpflag_ptr, double *val0_ptr, double *val1_ptr);
+errortype squaresolver(double a, double b, double c, rootnum *outpflag_ptr, double *val0_ptr, double *val1_ptr);
 /* Выполняет вычисления для решения уравнения. Получает на ввод данные коэффиценты a, b, c,
     а также указатели outpflag_ptr - служебную переменную для функции output() и
     val0_ptr (= -b/2a) и val1_ptr (= sqrt(b^2 - 4ac)/2a ) -
@@ -44,7 +44,13 @@ errortype EquasionTester(void);
 /*Вызыывает функцию TestOneEq количество TestCounter раз и передает ей данные
     из Testdata.txt*/
 
-void TestOneEq(double dat[], rootnum outpfag_ref);
+errortype TestOneEq(double dat[], rootnum outpfag_ref);
 #endif /*MYFUNCTIONS_H*/
 
 rootnum Convertstringtoenum(char Enumstring[]);
+
+void ErrorProcessing(errortype Errorcode);
+
+errortype CMDProcessing(int argc, char *argv[]);
+
+errortype SolveStart();
