62229bf3482e2d10a7344b4f8d2c5d078435059a
MihailIlyushenkov <142346042+MihailIlyushenkov@users.noreply.github.com>
2023-08-24 10:05:50 +0300

    Update firsttask.cpp
    Теперь в main() только вызов функции обработки параметров командной строки и функция обработки ошибок.

--- firsttask.cpp
+++ firsttask.cpp
@@ -1,33 +1,21 @@
 #include <stdio.h>
 #include <math.h>
 #include "myfunctions.h"
+#include <string.h>
 
-
-// Сделать проверки на входе в каждую функцию
 // Почитать про doxygen
-// Фикс кодстайла ip => ptr, ... сделал
 // Make почитать
 
-int main(void)
+
+int main(int argc, char *argv[])
 {
-    errortype TestEr = NOERROR;
-    /* double a = 0, b = 0, c = 0, val0 = 0, val1 = 0;
-    rootnum outpflag;
-    int prec = 1;
+    errortype Errcode = NOERROR;
 
-    input(&a, &b, &c);
-    prec = precision_input();
-    eqsdef(a, b, c, &outpflag, &val0, &val1);
-    output(&val0, &val1, outpflag, prec); */
 
-    TestEr = EquasionTester();
+    Errcode = CMDProcessing(argc, argv);
 
-    if (TestEr != NOERROR)
-    {
-        printf("UNEXPECTED ERROR: code %d\n", TestEr);
-        printf("Error codes:\n");
-        printf("code 0 - no errors\ncode 1 - error of reading file with test data");
-    }
+    ErrorProcessing(Errcode);
+
     getchar();
 
     return 0;
